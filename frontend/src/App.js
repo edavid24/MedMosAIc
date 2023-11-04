07bb6dfe535f85d28d51ee06e1204394da7ddfbe
Daniel <element11sodium@gmail.com>
2023-11-04 15:34:26 -0400

    adding frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import WebRequestButton from './WebRequestButton';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Web Request Button</h1>
+      <WebRequestButton />
+    </div>
+  );
+}
+
+export default App;
