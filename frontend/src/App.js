c07dcdc59d61e6b66f0953f9759e239487e1afcc
vincent31545 <vincentlau31545@gmail.com>
2023-11-04 16:31:21 -0400

    hah merge conflict

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import WebRequestButton from './WebRequestButton';
+import NavBar from './navBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <h1>Web Request Button</h1>
       <WebRequestButton />
     </div>
