574a8eecb253dc1e65ed7246350e3b1fba50eb71
Daniel <element11sodium@gmail.com>
2023-11-04 18:19:37 -0400

    react go brrr

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< HEAD
 import WebRequestButton from './WebRequestButton';
 import NavBar from './navBar';
 
@@ -10,6 +11,49 @@ function App() {
       <WebRequestButton />
     </div>
   );
-}
+=======
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Link,
+} from "react-router-dom";
 
+import Patients from './pages/Patients';
+import Profile from './pages/Profile';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Patients />} />
+                <Route path="/profile/:id" element={<Profile/>} />
+            </Routes>
+        </BrowserRouter>
+    );
+>>>>>>> 9409cfe (react nonsense)
+}
 export default App;
+/*
+import React from 'react';
+import PatientCard from './components/PatientCard';
+import WebRequestButton from './components/WebRequestButton';
+import Logo from './components/Logo';
+
+function App() {
+    return (
+        <div className="App">
+            <Logo text="MedMosaic" />
+            <PatientCard
+                name="John Doe"
+                number="3B"
+                details="asfdafjeaoifejafj"
+            /><PatientCard
+                name="John Doe"
+                number="3B"
+                details="asfdafjeaoifejafj"
+            />
+        </div>
+    );
+}
+*/
---
+++ frontend/src/components/PatientCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import './PatientCard.css'; // Create the corresponding CSS file
+
+function PatientCard({ number, name, details }) {
+    return (
+        <Link to={`/profile/${name}`}>
+            <div className="patient-card">
+                <div className="patient-header">
+                    <div className="patient-number">{number}</div>
+                    <div className="patient-name">{name}</div>
+                </div>
+                <div className="patient-details">{details}</div>
+            </div>
+        </Link>
+    );
+}
+
+export default PatientCard;
