4742f38ed338e9cc8f7390776a0d545996fb67b5
vincent31545 <vincentlau31545@gmail.com>
2023-11-04 21:03:34 -0400

    styling patientcard

--- frontend/src/components/PatientCard.js
+++ frontend/src/components/PatientCard.js
@@ -3,16 +3,13 @@ import { Link } from "react-router-dom";
 import './PatientCard.css'; // Create the corresponding CSS file
 
 function PatientCard({ number, name, details }) {
+    
     return (
         <Link to={`/profile/${name}`}>
             <div className="patient-card">
-                <div>
-                <div className="patient-header">
-                    <div className="patient-number">{number}</div>
-                    <div className="patient-name">{name}</div>
-                </div>
+                <div className="patient-name">{name}</div>
+                <div className="patient-number">{number}</div>
                 <div className="patient-details">{details}</div>
-                </div>
             </div>
         </Link>
     );
