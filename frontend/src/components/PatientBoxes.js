3e4e65507f63552eaddb9116aef6caee2588d509
vincent31545 <vincentlau31545@gmail.com>
2023-11-05 01:30:32 -0400

    styling the page to proper divs

--- frontend/src/components/PatientBoxes.js
+++ frontend/src/components/PatientBoxes.js
@@ -13,8 +13,8 @@ function PatientBoxes({ patientData }) {
         <div>
             <div id="patient-name">{patientData.name}</div>
             <div className="triplet-container"> 
-                <div className="triplet patient-mosaic">
-                    <h2>Patient Information</h2>
+                <div className="halfTriplet">
+                    <h2>Information</h2>
                     {Object.keys(patientData.info).map((property, index) => (
                         <p
                             key={index}
@@ -22,20 +22,25 @@ function PatientBoxes({ patientData }) {
                             <span style={{"font-weight" : "bold"}}>{property}:</span> {patientData.info[property]}
                         </p>
                     ))}
-                    <h2>Medical History</h2>
+                </div>
+                
+                <div className={`triplet ${isHidden ? 'hide' : ''}`} id='editor'>
+                        <button onClick={handleButtonClick}>DISAPPEAR</button>
+                        <WebRequestButton />
+                </div>
+                <div className='triplet'>
+                    <Checklist />
+                </div>
+
+                <div className="halfTriplet last">
+                    <h2>History</h2>
                     <li>
                         <ul>
                             LEAN
                         </ul>
                     </li>
                 </div>
-                <div className='triplet'>
-                    <Checklist />
-                </div>
-                <div className={`triplet ${isHidden ? 'hide' : ''}`} id='editor'>
-                    <button onClick={handleButtonClick}>DISAPPEAR</button>
-                    <WebRequestButton />
-                </div>
+                
             </div>
         </div>
     );
