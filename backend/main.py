8f403faaca8a03690b3b192bdacb9f967dbce721
Josh Marion <JoshuaRM8118@gmail.com>
2023-11-04 23:34:56 -0400

    i think everyting works lol. Also have api key on client locally

--- backend/main.py
+++ backend/main.py
@@ -1,13 +1,86 @@
 import requests
+import config
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-api_key = "y3nMbdJqOAVaexHWXedyM9iYhmQJZ9uPHQqXi9jEj7kCCpDAbTRy6B0IGKJKaxvD"
+api_key = config.api_key
+
+#@app.route('/addpatients', methods=['PATCH'])
+#def put_patient():
+#    return "hi"
+
+
+@app.route('/patients/<patient_id>', methods=['PUT'])
+def update_patient(patient_id):
+    data = request.get_json()
+    
+    url = "https://data.mongodb-api.com/app/data-eiten/endpoint/data/v1/action/replaceOne"
+    headers = {
+    "apiKey": api_key,
+    "Content-Type": "application/ejson",
+    "Accept": "application/json"
+    }
+
+    payload = {
+    "dataSource": "Cluster0",
+    "database": "test_medical_data",
+    "collection": "people",
+    "filter": {"patient_id": patient_id},
+    "replacement": data
+    }
+
+    
+    # Unfortunately the mongodb api seems to return 200 no matter what,
+    # Even if nothing was found to be updated. As a result, I have to
+    # check to manually to see if anything was changed in the json
+    response = requests.post(url, headers=headers, json=payload)
+    tmp = response.json()
+    if tmp["matchedCount"] == 1 and tmp["modifiedCount"] == 1:
+        return "Success! Status code is " + str(response.status_code)
+    else:
+        return "Failed to update"
+
+# Delete a Patient (DELETE)
+@app.route('/patients/<patient_id>', methods=['DELETE'])
+def delete_patient(patient_id):
+
+    url = "https://data.mongodb-api.com/app/data-eiten/endpoint/data/v1/action/deleteOne"
+    headers = {
+    "apiKey": api_key,
+    "Content-Type": "application/ejson",
+    "Accept": "application/json"
+    }
+
+    payload = {
+    "dataSource": "Cluster0",
+    "database": "test_medical_data",
+    "collection": "people",
+    "filter": {"patient_id": patient_id}
+    # "filter": { "_id": { "$oid": patient_id } }
+    }
+
+    # Send the DELETE request
+    response = requests.post(url, headers=headers, json=payload)
+    print(response)
+
+    tmp = response.json()
+    #return str(tmp)
+
+    # Unfortunately the mongodb api seems to return 200 no matter what,
+    # Even if nothing was found to be deleted. As a result, I have to
+    # check to manually to see if anything was changed in the json
+    response = requests.post(url, headers=headers, json=payload)
+    tmp = response.json()
+    if tmp["deletedCount"] == 1:
+        return "Patient was successfully deleted"
+    else:
+        return "Failed to delete patient"
+
 
 
 @app.route('/addpatients', methods=['POST'])
-def put_patient():
+def insert_patient():
 
     print(request)
 
@@ -76,6 +149,8 @@ def get_patients():
     return data
 
 
+
+
 # y3nMbdJqOAVaexHWXedyM9iYhmQJZ9uPHQqXi9jEj7kCCpDAbTRy6B0IGKJKaxvD
 # Set as variable before pushing to github
 
@@ -94,7 +169,7 @@ def get_patient(patient_id):
     "dataSource": "Cluster0",
     "database": "test_medical_data",
     "collection": "people",
-    "filter": { "patient_id": "444222222" }
+    "filter": { "patient_id": patient_id }
     }
 
     # Send the POST request
@@ -108,6 +183,8 @@ def get_patient(patient_id):
         print(f"Request failed with status code: {response.status_code}")
 
     return data
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
